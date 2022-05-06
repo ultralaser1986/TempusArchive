103d084b80ce81a4d1d2e805f4318e324b0b9969
bakapear <proxybomb@gmail.com>
2022-05-06 18:10:00 +0200

    use custom list file format

--- src/update.js
+++ src/update.js
@@ -2,6 +2,9 @@ let fs = require('fs')
 let ph = require('path')
 let dp = require('despair')
 
+let YouTube = require('./youtube')
+let yt = new YouTube('./data/keys.json')
+
 let base = 'https://tempus.xyz/api'
 let MAX_MAPS = 800
 let ZONES = ['bonus', 'trick']
@@ -16,22 +19,46 @@ let TempusAPI = {
 }
 
 async function updateRecordsFile (file) {
-  let RECORDS = {}
+  let RECORDS = []
   for (let i = 0; i < MAX_MAPS; i++) {
     let map = await TempusAPI.getMap(i)
     if (map) {
       for (let zone of ZONES) {
         for (let i = 0; i < map.zone_counts[zone]; i++) {
           let rec = await TempusAPI.getRecords(map.map_info.id, zone, i + 1, 1)
-          RECORDS[rec.zone_info.id] = {
-            3: rec.results.soldier[0]?.id || null,
-            4: rec.results.demoman[0]?.id || null
-          }
+          let s = rec.results.soldier[0]?.id
+          let d = rec.results.demoman[0]?.id
+          if(s) RECORDS.push(`S_${rec.zone_info.id} ${s}`)
+          if(d) RECORDS.push(`D_${rec.zone_info.id} ${d}`)
         }
       }
     }
   }
-  fs.writeFileSync(file, JSON.stringify(RECORDS))
+  fs.writeFileSync(file, RECORDS.join("\n"))
 }
 
-updateRecordsFile(ph.resolve(__dirname, 'data', 'records.json'))
+async function updateUploadsFile (file) {
+  let UPLOADS = []
+
+  let loopVids = async next => {
+    let res = await yt.listVideos(next)
+    
+    for(let item of res.items) {
+      let tfclass = item.title.match(/^\[(\w)\]/)[1]
+      let [,record, zone] = item.description.match(/records\/(\d+)\/(\d+)/).map(x => Number(x))
+      
+      UPLOADS.push(`${tfclass}_${zone} ${record} ${item.videoId}`)
+    }
+
+    if(res.next) await loopVids(next)
+  }
+  await loopVids()
+
+  fs.writeFileSync(file, UPLOADS.join("\n"))
+}
+
+async function main() {
+  //await updateRecordsFile(ph.resolve(__dirname, 'data', 'records.list'))
+  await updateUploadsFile(ph.resolve(__dirname, 'data', 'uploads.list'))
+}
+main()
