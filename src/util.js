d53cda05b98bbf1e82e663d6a56d97b1d4b33ae1
bakapear <proxybomb@gmail.com>
2023-01-26 11:06:44 +0100

    fix formatTime

--- src/util.js
+++ src/util.js
@@ -32,7 +32,10 @@ module.exports = {
 
     let t = [h, m, s].filter(x => x !== null).map((x, i) => (i !== 0 && x < 10 && x !== '00') ? '0' + x : x)
 
-    return (invert ? '-' : '') + t.join(':') + (decimals ? '.' + (ms % 1).toString().slice(2, 4 + decimals).slice(2) : '')
+    let decs = (ms % 1).toString().slice(2)
+    decs = decs + '0'.repeat(16 - decs.length)
+
+    return (invert ? '-' : '') + t.join(':') + (decimals ? '.' + decs.slice(0, decimals) : '')
   },
   maxLen (str, len) {
     if (str.length > len) str = str.slice(0, len - 3).trim() + '...'
